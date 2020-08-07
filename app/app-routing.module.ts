5dfa53f7cbc70cb2cb0f67eec6792d86e39283b3
Makham Satya Pavan Kumar Gupta <47024749+mspavan1234@users.noreply.github.com>
2020-08-07 11:38:17 +0530

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { hostViewClassName } from '@angular/compiler';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register-success', component: RegisterSuccessComponent},
+  {path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
